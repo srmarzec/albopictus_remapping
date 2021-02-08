364af5d82b566a564b85056a7ca0b5b75206e36a
srmarzec <srmarzec@users.noreply.github.com>
2021-02-08 17:03:51 -0500

    Create Rename_embryo.sh

---
+++ misc/sra_accession/Rename_embryo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#This is just a bunch of renaming commands to change sra accession numbers into actual usable info
+
+mv SRR458462_1.fastq.gz DI_72h_rep1_1.fastq.gz
+mv SRR458463_1.fastq.gz DI_72h_rep2_1.fastq.gz
+mv SRR458464_1.fastq.gz DI_72h_rep3_1.fastq.gz
+mv SRR458465_1.fastq.gz DI_135h_rep1_1.fastq.gz
+mv SRR458466_1.fastq.gz DI_135h_rep2_1.fastq.gz
+mv SRR458467_1.fastq.gz DI_135h_rep3_1.fastq.gz
+mv SRR458468_1.fastq.gz NDI_72h_rep1_1.fastq.gz
+mv SRR458469_1.fastq.gz NDI_72h_rep2_1.fastq.gz
+mv SRR458470_1.fastq.gz NDI_72h_rep3_1.fastq.gz
+mv SRR458471_1.fastq.gz NDI_135h_rep1_1.fastq.gz
+mv SRR458472_1.fastq.gz NDI_135h_rep2_1.fastq.gz
+mv SRR458473_1.fastq.gz NDI_135h_rep3_1.fastq.gz
+mv SRR458462_2.fastq.gz DI_72h_rep1_2.fastq.gz
+mv SRR458463_2.fastq.gz DI_72h_rep2_2.fastq.gz
+mv SRR458464_2.fastq.gz DI_72h_rep3_2.fastq.gz
+mv SRR458465_2.fastq.gz DI_135h_rep1_2.fastq.gz
+mv SRR458466_2.fastq.gz DI_135h_rep2_2.fastq.gz
+mv SRR458467_2.fastq.gz DI_135h_rep3_2.fastq.gz
+mv SRR458468_2.fastq.gz NDI_72h_rep1_2.fastq.gz
+mv SRR458469_2.fastq.gz NDI_72h_rep2_2.fastq.gz
+mv SRR458470_2.fastq.gz NDI_72h_rep3_2.fastq.gz
+mv SRR458471_2.fastq.gz NDI_135h_rep1_2.fastq.gz
+mv SRR458472_2.fastq.gz NDI_135h_rep2_2.fastq.gz
+mv SRR458473_2.fastq.gz NDI_135h_rep3_2.fastq.gz
