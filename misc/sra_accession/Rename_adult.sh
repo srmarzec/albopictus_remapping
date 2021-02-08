4c0810941b952398f4eaf445aa607f44a7826f0e
srmarzec <srmarzec@users.noreply.github.com>
2021-02-08 17:05:15 -0500

    Create Rename_adult.sh

---
+++ misc/sra_accession/Rename_adult.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#This is just a bunch of renaming commands to change sra accession numbers into actual relevant info
+
+mv SRR1663685_1.fastq.gz SD_BM_rep1_1.fastq.gz
+mv SRR1663687_1.fastq.gz SD_BM_rep2_1.fastq.gz
+mv SRR1663689_1.fastq.gz SD_BM_rep3_1.fastq.gz
+mv SRR1663697_1.fastq.gz SD_BM_rep4_1.fastq.gz
+mv SRR1663700_1.fastq.gz SD_NB_rep1_1.fastq.gz
+mv SRR1663703_1.fastq.gz SD_NB_rep2_1.fastq.gz
+mv SRR1663707_1.fastq.gz SD_NB_rep3_1.fastq.gz
+mv SRR1663709_1.fastq.gz SD_NB_rep4_1.fastq.gz
+mv SRR1663754_1.fastq.gz LD_BM_rep1_1.fastq.gz
+mv SRR1663769_1.fastq.gz LD_BM_rep2_1.fastq.gz
+mv SRR1663843_1.fastq.gz LD_BM_rep3_1.fastq.gz
+mv SRR1663911_1.fastq.gz LD_BM_rep4_1.fastq.gz
+mv SRR1663913_1.fastq.gz LD_NB_rep1_1.fastq.gz
+mv SRR1663916_1.fastq.gz LD_NB_rep2_1.fastq.gz
+mv SRR1664190_1.fastq.gz LD_NB_rep3_1.fastq.gz
+mv SRR1664192_1.fastq.gz LD_NB_rep4_1.fastq.gz
+mv SRR1663685_2.fastq.gz SD_BM_rep1_2.fastq.gz
+mv SRR1663687_2.fastq.gz SD_BM_rep2_2.fastq.gz
+mv SRR1663689_2.fastq.gz SD_BM_rep3_2.fastq.gz
+mv SRR1663697_2.fastq.gz SD_BM_rep4_2.fastq.gz
+mv SRR1663700_2.fastq.gz SD_NB_rep1_2.fastq.gz
+mv SRR1663703_2.fastq.gz SD_NB_rep2_2.fastq.gz
+mv SRR1663707_2.fastq.gz SD_NB_rep3_2.fastq.gz
+mv SRR1663709_2.fastq.gz SD_NB_rep4_2.fastq.gz
+mv SRR1663754_2.fastq.gz LD_BM_rep1_2.fastq.gz
+mv SRR1663769_2.fastq.gz LD_BM_rep2_2.fastq.gz
+mv SRR1663843_2.fastq.gz LD_BM_rep3_2.fastq.gz
+mv SRR1663911_2.fastq.gz LD_BM_rep4_2.fastq.gz
+mv SRR1663913_2.fastq.gz LD_NB_rep1_2.fastq.gz
+mv SRR1663916_2.fastq.gz LD_NB_rep2_2.fastq.gz
+mv SRR1664190_2.fastq.gz LD_NB_rep3_2.fastq.gz
+mv SRR1664192_2.fastq.gz LD_NB_rep4_2.fastq.gz
