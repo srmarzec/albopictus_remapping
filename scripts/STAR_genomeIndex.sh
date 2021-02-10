f70898cad3584e738aa32644496e7cf1c49839cb
srmarzec <srmarzec@users.noreply.github.com>
2021-02-10 15:24:04 -0500

    Update STAR_genomeIndex.sh

--- scripts/STAR_genomeIndex.sh
+++ scripts/STAR_genomeIndex.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=STAR_genomeIndex --output=%x.%j.out
 #SBATCH --mail-type=END,FAIL --mail-user=[netID]@georgetown.edu
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=100G
 
 #-----------------------------------------------------------------------------#
 # This script makes index of the ref genome using STAR #
@@ -13,7 +13,7 @@ module load star/2.7.1a
 #- Set variables ----------------------------------------------------------------#
 
 refgen_dir=[path to ref genome and gff3 file]
-refgen_index=[path to directory for indexed genome (should be empty based on STAR manual)]
+refgen_index=[path to directory of new indexed genome]
 
 
 #- RUN STAR----------------------------------------------------------------#
