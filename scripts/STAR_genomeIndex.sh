11a6790c6ed70a1112978df6ea73ca4e5899f3f2
srmarzec <srmarzec@users.noreply.github.com>
2021-02-10 12:48:24 -0500

    Create STAR_genomeIndex.sh

---
+++ scripts/STAR_genomeIndex.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=STAR_genomeIndex --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=[netID]@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=4G
+
+#-----------------------------------------------------------------------------#
+# This script makes index of the ref genome using STAR #
+#-----------------------------------------------------------------------------#
+
+module load star/2.7.1a
+
+#- Set variables ----------------------------------------------------------------#
+
+refgen_dir=[path to ref genome and gff3 file]
+refgen_index=[path to directory for indexed genome (should be empty based on STAR manual)]
+
+
+#- RUN STAR----------------------------------------------------------------#
+
+STAR --runMode genomeGenerate \
+        --genomeDir ${refgen_index} \
+        --genomeFastaFiles ${refgen_dir}/aedes_albopictus_AalbF3.fa \
+        --sjdbGTFfile ${refgen_dir}/aedes_albopictus_AalbF3.gff3
+
+#- FIN -----------------------------------------------------------------------#
