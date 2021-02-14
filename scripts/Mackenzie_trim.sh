ec59b7ae9297b4f2a59595afa8220e46e7ffb24e
mlp134 <78369439+mlp134@users.noreply.github.com>
2021-02-14 15:32:16 -0500

    Create Mackenzie_trim.sh

---
+++ scripts/Mackenzie_trim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=trim_embryo --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=mlp134@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=4G
+#-----------------------------------------------------------------------------#
+# This script runs trimmomatic to clean up raw reads for the embryo sequences #
+#-----------------------------------------------------------------------------#
+#- Set variables ----------------------------------------------------------------#
+raw_dir=/home/mlp134/RNAseqproject/raw_embryo_data
+trim_dir=/home/mlp134/RNAseqproject/trim_embryo_output
+trim=/home/mlp134/RNAseqproject/Trimmomatic-0.39/trimmomatic-0.39.jar
+adapter=/home/mlp134/RNAseqproject/Trimmomatic-0.39/adapters/TruSeq3-PE-2.fa:2:30:10
+#- RUN Trimmomatic----------------------------------------------------------------#
+files=(${raw_dir}/*_1.fastq.gz)
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} _1.fastq.gz`
+java -Xmx2G -jar ${trim} PE \
+${raw_dir}/${base}_1.fastq.gz \
+${raw_dir}/${base}_2.fastq.gz \
+${trim_dir}/${base}_1_PE.fastq.gz ${trim_dir}/${base}_1_SE.fastq.gz \
+${trim_dir}/${base}_2_PE.fastq.gz ${trim_dir}/${base}_2_SE.fastq.gz \
+          ILLUMINACLIP:${adapter} \
+          HEADCROP:15 \
+          SLIDINGWINDOW:4:15 \
+          MINLEN:50
+done
+#- FIN -----------------------------------------------------------------------#
