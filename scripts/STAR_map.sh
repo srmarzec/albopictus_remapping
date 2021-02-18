c7be6160f7b417253a2f4e4d2497328272b70daa
srmarzec <srmarzec@users.noreply.github.com>
2021-02-18 16:18:23 -0500

    Create STAR_map.sh

---
+++ scripts/STAR_map.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=STAR --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=[netID]@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=100G
+
+#-----------------------------------------------------------------------------#
+# This script maps reads to the ref genome using STAR #
+#-----------------------------------------------------------------------------#
+
+module load star/2.7.1a
+
+#- Set variables ----------------------------------------------------------------#
+
+trim_dir=[path to the trimmed files]
+
+out_dir=[path to the output directory]
+
+refgen_dir=[path to reference genome index]
+
+
+#- RUN STAR----------------------------------------------------------------#
+
+files=(${trim_dir}/*_1_PE.fastq.gz)
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} _1_PE.fastq.gz`
+STAR --genomeDir ${refgen_dir} \
+        --readFilesCommand gunzip -c \
+        --readFilesIn ${trim_dir}/${base}_1_PE.fastq.gz ${trim_dir}/${base}_2_PE.fastq.gz \
+        --outFileNamePrefix ${out_dir}/${base}_ \
+        --outSAMtype BAM SortedByCoordinate 
+done
+
+#- FIN -----------------------------------------------------------------------#
