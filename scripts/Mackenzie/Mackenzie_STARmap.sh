27a7794f3424b56250c22f060cf93c56b667a621
mlp134 <78369439+mlp134@users.noreply.github.com>
2021-02-22 21:14:51 -0500

    Update Mackenzie_STARmap.sh

--- scripts/Mackenzie/Mackenzie_STARmap.sh
+++ scripts/Mackenzie/Mackenzie_STARmap.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=STAR --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=mlp134@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=100G
 
+#-----------------------------------------------------------------------------#
+# This script maps reads to the ref genome using STAR #
+#-----------------------------------------------------------------------------#
+
+module load star/2.7.1a
+
+#- Set variables ----------------------------------------------------------------#
+
+trim_dir=[path to the trimmed files]
+
+out_dir=[path to the sam output directory]
+
+refgen_dir=[path to reference genome index]
+
+
+#- RUN STAR----------------------------------------------------------------#
+
+files=(${trim_dir}/*_1_PE.fastq.gz)
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} _1_PE.fastq.gz`
+STAR --genomeDir ${refgen_dir} \
+        --readFilesCommand gunzip -c \
+        --readFilesIn ${trim_dir}/${base}_1_PE.fastq.gz ${trim_dir}/${base}_2_PE.fastq.gz \
+        --outFileNamePrefix ${out_dir}/${base}_  
+done
+
+#- FIN -----------------------------------------------------------------------#
