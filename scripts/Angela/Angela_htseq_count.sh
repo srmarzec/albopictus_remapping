317803c725194e56b11725d73b54a16168cfb4ae
AngelaZhou779 <78465068+AngelaZhou779@users.noreply.github.com>
2021-03-04 23:10:29 -0500

    Update Angela_htseq_count.sh

--- scripts/Angela/Angela_htseq_count.sh
+++ scripts/Angela/Angela_htseq_count.sh
@@ -10,12 +10,12 @@ source /home/zz220/python-env/bin/activate
 #- Set variables ----------------------------------------------------------------#
 bam_dir=/home/zz220/albopictus_remap/bam_dir
 count_dir=/home/zz220/albopictus_remap/htseqcounts_dir
-htseq=/home/zz220/albopictus_remap/scripts/htseqPhython.SBATCH 
+htseq=/home/zz220/python-env/bin/htseq-count
 ref=/home/zz220/albopictus_remap/albopictus_genome/aedes_albopictus_AalbF3.gff3
 #- RUN fastqc ----------------------------------------------------------------#
 files=(${bam_dir}/*_Aligned.out.bam)
 for file in ${files[@]}
-do`
+do
 base=`basename ${file} _Aligned.out.bam`
 ${htseq} -f bam -r pos -s no -t exon -i gene ${bam_dir}/${base}_Aligned.out.bam ${ref} > ${count_dir}/${base}_htseqCount
 done
