d542c7b59f1b0a7c147107f1aa8dbff433eca1f3
AngelaZhou779 <78465068+AngelaZhou779@users.noreply.github.com>
2021-03-15 20:26:59 -0400

    Update and rename DESeq.R to Angela_DESeq.R

--- scripts/Angela/Angela_DESeq.R
+++ scripts/Angela/Angela_DESeq.R
@@ -8,24 +8,25 @@
 # BiocManager::install("GOplot")
 
 #Load Libraries
+#We either just installed these or installed these in the last script (countreads)
 library(DESeq2); library(ggplot2); library(tidyverse); library(EnhancedVolcano); library(GOplot); library(pheatmap)
 
-# Set working directory to source file location
+# Set working directory to source file location (scripts folder)
 
-#Choose directory with htseq-count data
-directory<-("../data")
+#Choose directory with htseq-count data (relative to scripts folder)
+directory<-("../Data")
 
 #Create the sample table (this could alternatively be made externally and read in)
 #grabbing all the files in the directory with NB and setting that as sampleFiles
-sampleFiles <- grep("11d", list.files(directory), value=T) #this is pulling all files from the directory with the age specified
+sampleFiles <- grep("NB", list.files(directory), value=T) #this is pulling all files from the directory with the NB (no blood meal) because we want to sperate comparisons by blood meal and no blood meal
 sampleNames <- sub("_htseqCount","",sampleFiles) #this is removing the ending of the files to better represent the sample names
 sampleConditions <- sub("_.*","", sampleFiles) #to get conditions (long days/short day i.e. non diapause/diapause) I pull everything from before the first underscore (which I know is the diapause status)
 
-#a factor=levels tfor a certain variable; telling deseq it is a variable. for example, 1=LD and 2=ND and then ask R to compare 1 and 2
+#a factor=levels for a certain variable; telling deseq it is a variable. for example, 1=LD and 2=ND and then ask R to compare 1 and 2
 sampleTable <- data.frame(sampleName = sampleNames,
                           fileName = sampleFiles,
                           condition = sampleConditions)
-str(sampleTable)
+str(sampleTable) #str allows you to view the internal structure of an R object
 sampleTable$condition <- factor(sampleTable$condition)
 
 # Make the DESeq dataset
@@ -39,7 +40,7 @@ dds
 keep <- rowSums(counts(dds)) >= 10
 dds <- dds[keep,]
 
-#Relevel the condition for what to compare to. Likely not important in our case with two conditions, but you would want everything compared to the control. (Default is first condition alphabetically)
+#Relevel the condition for what to compare to. Likely not important in our case with two conditions, but you would want everything compared to the control. (Default is first condition alphabetically, in this case, it happens to be the control that we want: LD or nondiapause)
 dds$condition <- relevel(dds$condition, ref = "LD")
 
 # A bit of quality control
@@ -47,6 +48,7 @@ dds$condition <- relevel(dds$condition, ref = "LD")
 # PCA longest axis, explains most variation of the data and seperates it based on that
 librarySizes <- colSums(counts(dds))
 
+#saved image as "barplot of library sizes"; it gives you an iea of count distribution across samples
 barplot(librarySizes, 
         names=names(librarySizes), 
         las=2, ylim = c(0,1.6e+07),
@@ -58,6 +60,8 @@ head(logcounts)
 #Is there any difference between per gene counts for each of the sample groups?
 statusCol <- as.numeric(factor(dds$condition)) + 1  # make a colour vector
 
+#difference between per gene counts for each of the sample groups
+#saved image as logcounts
 boxplot(logcounts, 
         xlab="", 
         ylab="Log2(Counts)",
@@ -68,7 +72,7 @@ boxplot(logcounts,
 abline(h=median(as.matrix(logcounts)), col="blue")
 
 #Looking at PCA of the data - Do treatments cluster together?
-rlogcounts <- rlog(counts(dds))#transforming data to make it approximately homoskedastic, n < 30 so rlog is better
+rlogcounts <- rlog(counts(dds)) #transforming data to make it approximately homoskedastic, n < 30 so rlog is better
 
 data_for_PCA <- t(rlogcounts)
 dim(data_for_PCA)
@@ -83,7 +87,8 @@ ggplot(pcDat_df, aes(x = PC1, y = PC2, col = Condition)) +
   theme_bw()
 
 # Transform normalized counts using the rlog function ("RNASEQ20_Day3_HandsOn.pdf")
-# see what percentage of the variance can be explained by PC1
+# see what percentage of the variance can be explained by PC1 (LD vs SD i.e. nondiapause vs diapause)
+#saved image as PCA counts
 rld <- rlog(dds, blind=TRUE)
 plotPCA(rld, intgroup="condition")
 
@@ -94,7 +99,7 @@ rld_mat <- assay(rld) #retrieve matrix from the rld object
 # compute pairwise correlation values for samples
 rld_cor <- cor(rld_mat)
 # plot the correlation c=values as a heatmap
-pheatmap(rld_cor)
+heatmap(rld_cor)
 
 # # Look at the difference in total reads from the different samples
 # dds <- estimateSizeFactors(dds)
@@ -112,7 +117,7 @@ res
 
 # Apparently, it is common to shrink the log fold change estimate for better visualization and ranking of genes. Often, lowly expressed genes tend to have relatively high levels of variability so shrinking can reduce this.
 resultsNames(dds)
-res_LFC <- lfcShrink(dds, coef="condition_D_vs_ND", type="apeglm")
+res_LFC <- lfcShrink(dds, coef="condition_SD_vs_LD", type="apeglm")
 res_LFC
 
 # Order the table by smallest p value
@@ -151,17 +156,17 @@ sig_res <- res_LFC %>%
 
 # Write out a table of these significant differentially expressed genes
 write.csv(select(sig_res, gene, log2FoldChange, padj), 
-            file="../misc/11D_DvND_LFCshrink_padj.txt", col.names = F, row.names = F)
+            file="../misc/NB_LDvSD_LFCshrink_padj.txt", col.names = F, row.names = F)
 
 # Write out just the gene names for later analysis in KEGG
 write.table(sig_res %>% select(gene), 
-            file="../misc/11D_DvND_test.txt", col.names = F, row.names = F, quote = F)
+            file="../misc/NB_LDvSD_test.txt", col.names = F, row.names = F, quote = F)
 
 
 ####################
-# Running through another dataset (21days)
+# Running through another dataset (BM)
 #Create the sample table (this could alternatively be made externally and read in)
-sampleFiles <- grep("21d", list.files(directory), value=T) #this is pulling all files from the directory with the age specified
+sampleFiles <- grep("BM", list.files(directory), value=T) #this is pulling all files from the directory with the age specified
 sampleNames <- sub("_htseqCount","",sampleFiles) #this is removing the ending of the files to better represent the sample names
 sampleConditions <- sub("_.*","", sampleFiles) #to get conditions I pull everything from before the first underscore (which I know is the diapause status)
 
@@ -172,33 +177,33 @@ str(sampleTable)
 sampleTable$condition <- factor(sampleTable$condition)
 
 # Make the DESeq dataset
-dds_21d <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+dds_BM <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
                                   directory = directory,
                                   design = ~ condition)
-dds_21d
+dds_BM
 
 #DESeq recommends a pre-filtering step to reduce memory size and increase speed. They suggest keeping only rows which have 10 reads total
 keep <- rowSums(counts(dds_21d)) >= 10
-dds_21d <- dds_21d[keep,]
+dds_BM <- dds_BM [keep,]
 
 #Relevel the condition for what to compare to. Likely not important in our case with two conditions, but you would want everything compared to the control. (Default is first condition alphabetically)
-dds_21d$condition <- relevel(dds_21d$condition, ref = "ND")
+dds_BM$condition <- relevel(dds_BM$condition, ref = "LD")
 
 
 #Run DESeq
-dds_21d <- DESeq(dds_21d)
-res_21d <- results(dds_21d)
-res_21d
+dds_BM <- DESeq(dds_BM)
+res_BM <- results(dds_BM)
+res_BM
 
 # Apparently, it is common to shrink the log fold change estimate for better visualization and ranking of genes. Often, lowly expressed genes tend to have relatively high levels of variability so shrinking can reduce this.
-resultsNames(dds_21d)
-res_LFC_21d <- lfcShrink(dds_21d, coef="condition_D_vs_ND", type="apeglm")
-res_LFC_21d
+resultsNames(dds_BM)
+res_LFC_BM <- lfcShrink(dds_BM, coef="condition_SD_vs_LD", type="apeglm")
+res_LFC_BM
 
 
 # Volcano plot of the lfcShrink data. For various setting try: `browseVignettes("EnhancedVolcano")`
-EnhancedVolcano(res_LFC_21d,
-                lab = rownames(res_LFC_21d),
+EnhancedVolcano(res_LFC_BM,
+                lab = rownames(res_LFC_BM),
                 x = 'log2FoldChange',
                 y = 'pvalue',
                 selectLab = NA,
@@ -211,93 +216,26 @@ EnhancedVolcano(res_LFC_21d,
                 labSize = 5.0)
 
 # Can look at which of the result are significant and have a high enough log2 fold change
-sig_res_21d <- res_LFC_21d %>%
+sig_res_BM <- res_LFC_BM %>%
   data.frame() %>%
   rownames_to_column(var="gene") %>% 
   as.data.frame() %>% 
   filter(padj < 0.05, abs(log2FoldChange) > 0.58)
 
 # Write out a table of these significant differentially expressed genes
-write.csv(select(sig_res_21d, gene, log2FoldChange, padj), 
-          file="../misc/21D_DvND_LFCshrink_padj.txt", row.names = F)
-
-
-####################
-# Running through another dataset (40days)
-#Create the sample table (this could alternatively be made externally and read in)
-sampleFiles <- grep("40d", list.files(directory), value=T) #this is pulling all files from the directory with the age specified
-sampleNames <- sub("_htseqCount","",sampleFiles) #this is removing the ending of the files to better represent the sample names
-sampleConditions <- sub("_.*","", sampleFiles) #to get conditions I pull everything from before the first underscore (which I know is the diapause status)
-
-sampleTable <- data.frame(sampleName = sampleNames,
-                          fileName = sampleFiles,
-                          condition = sampleConditions)
-str(sampleTable)
-sampleTable$condition <- factor(sampleTable$condition)
-
-# Make the DESeq dataset
-dds_40d <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
-                                      directory = directory,
-                                      design = ~ condition)
-dds_40d
-
-#DESeq recommends a pre-filtering step to reduce memory size and increase speed. They suggest keeping only rows which have 10 reads total
-keep <- rowSums(counts(dds_40d)) >= 10
-dds_40d <- dds_40d[keep,]
-
-#Relevel the condition for what to compare to. Likely not important in our case with two conditions, but you would want everything compared to the control. (Default is first condition alphabetically)
-dds_40d$condition <- relevel(dds_40d$condition, ref = "ND")
-
-
-#Run DESeq
-dds_40d <- DESeq(dds_40d)
-res_40d <- results(dds_40d)
-res_40d
-
-# Apparently, it is common to shrink the log fold change estimate for better visualization and ranking of genes. Often, lowly expressed genes tend to have relatively high levels of variability so shrinking can reduce this.
-resultsNames(dds_40d)
-res_LFC_40d <- lfcShrink(dds_40d, coef="condition_D_vs_ND", type="apeglm")
-res_LFC_40d
-
-
-# Volcano plot of the lfcShrink data. For various setting try: `browseVignettes("EnhancedVolcano")`
-EnhancedVolcano(res_LFC_40d,
-                lab = rownames(res_LFC_40d),
-                x = 'log2FoldChange',
-                y = 'pvalue',
-                selectLab = NA,
-                #drawConnectors = TRUE,
-                xlim = c(-1.5, 1.5),
-                ylim = c(0,30),
-                pCutoff = 10e-6,
-                FCcutoff = 0.58,
-                pointSize = 2.0,
-                labSize = 5.0)
-
-# Can look at which of the result are significant and have a high enough log2 fold change
-sig_res_40d <- res_LFC_40d %>%
-  data.frame() %>%
-  rownames_to_column(var="gene") %>% 
-  as.data.frame() %>% 
-  filter(padj < 0.05, abs(log2FoldChange) > 0.58)
-
-# Write out a table of these significant differentially expressed genes
-write.csv(select(sig_res_40d, gene, log2FoldChange, padj), 
-          file="../misc/40d_DvND_LFCshrink_padj.txt", row.names = F)
-
-
+write.csv(select(sig_res_BM, gene, log2FoldChange, padj), 
+          file="../misc/BM_SDvLD_LFCshrink_padj.txt", row.names = F)
 
 ##################
 # Making a venn diagram of the 3 different datasets DEGs
-dat_11 <- read.csv("../misc/11d_DvND_LFCshrink_padj.txt")
-dat_21 <- read.csv("../misc/21d_DvND_LFCshrink_padj.txt")
-dat_40 <- read.csv("../misc/40d_DvND_LFCshrink_padj.txt")
+dat_NB <- read.csv("../misc/NB_SDvLD_LFCshrink_padj.txt")
+dat_BM <- read.csv("../misc/BM_SDvLD_LFCshrink_padj.txt")
 
 
-l1 <- dat_11[, c(1,2)]
-l2 <- dat_21[, c(1,2)]
-l3 <- dat_40[, c(1,2)]
 
-VennDiag <- GOVenn(l1,l2,l3, label=c('11d','21d','40d'), plot = F)
+l1 <- dat_NB[, c(1,2)]
+l2 <- dat_BM[, c(1,2)]
+
+VennDiag <- GOVenn(l1,l2, label=c('NB','BM'), plot = F)
 print(VennDiag$plot)
 # The colors inside the labels: Red (up), Blue (down), and Yellow (contra-regulated). These colors can be changed but the order of the regulation (up, down, contra) starts at the top and goes clockwise.
