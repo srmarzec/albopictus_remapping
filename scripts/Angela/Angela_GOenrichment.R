323869138dc56b063c47c7889b671e1d3ed0f2f4
AngelaZhou779 <78465068+AngelaZhou779@users.noreply.github.com>
2021-04-11 20:35:06 -0400

    Update Angela_GOenrichment.R

--- scripts/Angela/Angela_GOenrichment.R
+++ scripts/Angela/Angela_GOenrichment.R
@@ -11,10 +11,12 @@
 # Load libraries
 library(topGO); library(Rgraphviz); library(formattable)
 
+setwd("~/OneDrive - Georgetown University/Differential Expression Analysis/Script")
+
 # Set up files to read in 
 annotations_file <- "../misc/FULL_GOannotations.txt" #Contains gene IDs and GO annotations. See specifics of GO-term annotation file above
-interesting_genes <- "../misc/21D_DvND_test.txt" #This file is a single column of gene IDs with no header
-project_description <- "larva 11d" #Name given for creating GO data object (something like "My project" or "larva 11d")
+interesting_genes <- "../misc/NB_LDvSD_test.txt" #This file is a single column of gene IDs with no header
+project_description <- "adult NB" #Name given for creating GO data object (something like "My project" or "larva 11d")
 
 # Read in GO annotations for genes
 geneID2GO <- readMappings(file = annotations_file) 
@@ -63,6 +65,17 @@ resultFisher <- runTest(myGOdata, algorithm="weight01", statistic="fisher")
 # Typing out resultFisher will give you a summary 
 resultFisher
 
+#Output:
+# Description: adult NB 
+# Ontology: MF 
+# 'weight01' algorithm with the 'fisher' test
+# 1406 GO terms scored: 17 terms with p < 0.01
+# Annotation data:
+#   Annotated genes: 13710 
+# Significant genes: 1250 
+# Min. no. of genes annotated to a GO: 1 
+# Nontrivial nodes: 680 
+
 # We can list the top ten significant results found:
 allRes <- GenTable(myGOdata, 
                    weightFisher = resultFisher, 
@@ -77,7 +90,97 @@ showSigOfNodes(myGOdata, score(resultFisher), firstSigNodes = 5, useInfo ='all')
 # Output description from the vingette: "The subgraph induced by the top 5 GO terms identified by the elim algorithm for scoring GO terms for enrichment. Rectangles indicate the 5 most significant terms. Rectangle color represents the relative significance, ranging from dark red (most significant) to bright yellow (least significant). For each node, some basic information is displayed. The first two lines show the GO identifier and a trimmed GO name. In the third line the raw p-value is shown. The forth line is showing the number of significant genes and the total number of genes annotated to the respective GO term."
 
 # The following prints out the above but as a PDF. Change the prefix in order to change the folder it prints to
-printGraph(myGOdata, resultFisher, firstSigNodes = 5, fn.prefix = "../output/tGO", useInfo = "all", pdfSW = TRUE)
+printGraph(myGOdata, resultFisher, firstSigNodes = 5, fn.prefix = "../output/tGO_NB", useInfo = "all", pdfSW = TRUE)
+
+# Find gene annotated with significant GO terms
+# This print all the genes from the gene universe that have the GO term
+myterms = c("GO:0043044") 
+mygenes <- genesInTerm(myGOdata, myterms)
+for (i in 1:length(myterms)) {
+  myterm <- myterms[i]
+  mygenesforterm <- mygenes[myterm][[1]]
+  mygenesforterm <- paste(mygenesforterm, collapse=',')
+  print(paste("Term",myterm,"genes:",mygenesforterm))
+}
+
+# Dataset 2
+# Set up files to read in 
+annotations_file <- "../misc/FULL_GOannotations.txt" #Contains gene IDs and GO annotations. See specifics of GO-term annotation file above
+interesting_genes <- "../misc/BM_SDvLD_test.txt" #This file is a single column of gene IDs with no header
+project_description <- "adult BM" #Name given for creating GO data object (something like "My project" or "larva 11d")
+
+# Read in GO annotations for genes
+geneID2GO <- readMappings(file = annotations_file) 
+
+# GO hierarchy
+# topGO uses a GO hierarchy that comes with the GO.db package, so we don't need to worry about his
+
+# Defining the genes of interest and what we will compare to ("gene universe")
+# The gene universe can be all the genes that have GO annotations (for us, this is the full set Sarah created)
+geneUniverse <- names(geneID2GO) 
+# Then we read in the list of interesting genes (for us, this is a set of significant DEGs)
+genesOfInterest <- read.table(interesting_genes,header=FALSE)
+genesOfInterest <- as.character(genesOfInterest$V1)
+# We need to say where the interesting genes appear in the gene universe set. This means creating an ojbect that lists out which genes in the gene universe are your genes of interest for topGO.
+geneList <- factor(as.integer(geneUniverse %in% genesOfInterest))
+names(geneList) <- geneUniverse
+
+# Create an R object that has all this data together
+myGOdata <- new("topGOdata", description=project_description, ontology="MF", allGenes=geneList,  annot = annFUN.gene2GO, gene2GO = geneID2GO)
+# The 'ontology' argument can be 'BP' (biological process), 'MF' (molecular function), or 'CC' (cellular component).
+# The 'description' argument has a short description of your project.
+# The 'allGenes' argument specifies all the genes in the 'gene universe', and which of those are  your genes of interest.
+# The 'annot' argument tells topGO how to map genes to GO annotations. 'annot'=annFUN.gene2GO means that the user provides gene-to-GO annotations, and we specify here that they are in object 'geneID2GO' (see above for how this was created).
+# An optional argument is 'nodeSize': this is used to prune the GO hierarchy, eg. nodesize=10 prunes the GO hierarchy, to remove terms which have less than 10 annotated genes.
+
+# Running the dataset name gives you a summary 
+myGOdata
+
+#graph(myGOdata)
+
+# The list of genes of interest can be accessed using the method sigGenes():
+sg <- sigGenes(myGOdata)
+str(sg)
+numSigGenes(myGOdata)
+
+
+#####
+
+# Performing enrichment tests of the GO data
+
+resultFisher <- runTest(myGOdata, algorithm="weight01", statistic="fisher")
+# Selecting 'algorithm=classic' means that the GO hierarchy isn't taken into account, so each GO term is tested independently. (Usually in fact we want to take the GO hierarchy into account, so would use algorithm='weight01' for example.)
+#resultKS <- runTest(myGOdata, algorithm = "classic", statistic = "ks")
+#resultKS.elim <- runTest(myGOdata, algorithm = "elim", statistic = "ks")
+
+# Typing out resultFisher will give you a summary 
+resultFisher
+
+# Description: adult BM 
+# Ontology: MF 
+# 'weight01' algorithm with the 'fisher' test
+# 1406 GO terms scored: 11 terms with p < 0.01
+# Annotation data:
+#   Annotated genes: 13710 
+# Significant genes: 612 
+# Min. no. of genes annotated to a GO: 1 
+# Nontrivial nodes: 432 
+
+# We can list the top ten significant results found:
+allRes <- GenTable(myGOdata, 
+                   weightFisher = resultFisher, 
+                   orderBy = "weightFisher", ranksOf = "weightFisher", topNodes = 10)
+
+formattable(allRes)
+
+
+
+# We can visualise the position of the statistically significant GO terms in the GO hierarchy by using the following functions:
+showSigOfNodes(myGOdata, score(resultFisher), firstSigNodes = 5, useInfo ='all')
+# Output description from the vingette: "The subgraph induced by the top 5 GO terms identified by the elim algorithm for scoring GO terms for enrichment. Rectangles indicate the 5 most significant terms. Rectangle color represents the relative significance, ranging from dark red (most significant) to bright yellow (least significant). For each node, some basic information is displayed. The first two lines show the GO identifier and a trimmed GO name. In the third line the raw p-value is shown. The forth line is showing the number of significant genes and the total number of genes annotated to the respective GO term."
+
+# The following prints out the above but as a PDF. Change the prefix in order to change the folder it prints to
+printGraph(myGOdata, resultFisher, firstSigNodes = 5, fn.prefix = "../output/tGO_BM", useInfo = "all", pdfSW = TRUE)
 
 # Find gene annotated with significant GO terms
 # This print all the genes from the gene universe that have the GO term
